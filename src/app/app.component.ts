99767975b5c541a413d3dfb013fb694dc31a727b
jramirez2602 <jorguitoramirez@gmail.com>
2024-11-17 18:04:57 -0400

    Creado proyecto con bootstrap integrado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JorgeComponent } from "./jorge/jorge.component";
+import { AngeloComponent } from "./angelo/angelo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JorgeComponent, AngeloComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'db-front';
+  title = 'Jorge te amo';
 }
