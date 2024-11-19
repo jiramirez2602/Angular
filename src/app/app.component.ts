8c5adf0d0fd62e120d48f1cbf77d6a3331e31501
jramirez2602 <jorguitoramirez@gmail.com>
2024-11-19 06:34:21 -0400

    actualización general creación de contenedores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { JorgeComponent } from "./jorge/jorge.component";
-import { AngeloComponent } from "./angelo/angelo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, JorgeComponent, AngeloComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { CuentaComponent } from './cuenta/cuenta.component';
+import { AdministracionComponent } from './administracion/administracion.component';
+import { PedidoComponent } from './pedido/pedido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'registro', component: RegistroComponent},
+    { path: 'login', component: LoginComponent},
+    { path: '', component: InicioComponent}, 
+    { path: 'cuenta', component: CuentaComponent},
+    { path: 'administracion', component: AdministracionComponent},
+    { path: 'pedido', component: PedidoComponent},
+    {path: '', redirectTo: '', pathMatch: 'full'}
+];
+
